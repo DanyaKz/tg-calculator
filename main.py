4650b2219ebe01f442288b94114698bbd011d76b
DanyaKz <78554802+DanyaKz@users.noreply.github.com>
2021-03-14 21:24:13 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from bot_init import Bot_init
 
-bot = Bot_init('1170215909:AAF0tMz2QSFzkmhoptykTqJom1FUrbR3-nw')
+bot = Bot_init('YOUR-TOKEN')
 bot.start_handler()
 bot.click_handler()
 bot.execute_bot()
